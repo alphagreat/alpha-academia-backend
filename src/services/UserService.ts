211dfb0197fd91b1c00c024d140ea47295147688
danielbontii <bontiidaniel97@gmail.com>
2023-02-18 12:42:54 +0000

    encrypted user passwords

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,8 +1,11 @@
 import { StatusCodes } from 'http-status-codes';
+import bcrypt from 'bcrypt';
+
 import { CustomError } from '../errors';
 import Bio from '../models/bio';
 import User from '../models/user';
 import { userSchema } from '../utils/validations';
+import { NewUserDTO } from '../types/newUserDTO';
 
 export class UserService {
   static async index(): Promise<User[]> {
@@ -14,9 +17,11 @@ export class UserService {
     return users;
   }
 
-  static async create(user: User): Promise<User> {
-    await userSchema.validateAsync(user, {abortEarly: false});
-    const userIsRegistered = await User.findOne({ where: { email: user.email } });
+  static async create(user: User): Promise<NewUserDTO> {
+    await userSchema.validateAsync(user, { abortEarly: false });
+    const userIsRegistered = await User.findOne({
+      where: { email: user.email }
+    });
 
     if (userIsRegistered) {
       throw new CustomError(
@@ -25,9 +30,17 @@ export class UserService {
       );
     }
 
+    const { BCRYPT_PASSWORD, SALT_ROUNDS } = process.env;
+
+    user.password = bcrypt.hashSync(
+      user.password + BCRYPT_PASSWORD,
+      parseInt(SALT_ROUNDS as string)
+    );
+
     const createdUser = await User.create<User>(user);
+
     await Bio.create({ userId: createdUser.id });
 
-    return createdUser;
+    return { id: createdUser.id, token: 'soon' };
   }
 }
